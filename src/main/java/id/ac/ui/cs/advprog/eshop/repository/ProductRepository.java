3672fb00542bb89bf94ffebde842b0717f917074
Kae20042005 <farrelilyasa200405@gmail,com>
2025-02-14 16:37:58 +0700

    adding edit product features

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,17 +6,39 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product) {
+        product.setProductId(UUID.randomUUID().toString());
         productData.add(product);
         return product;
     }
 
     public Iterator<Product> findAll() {
         return productData.iterator();
+    }
+
+    public Product findById(String id) {
+        return productData.stream()
+                .filter(product -> product.getProductId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("product not found"));
+    }
+
+    public Product edit(Product product) {
+        Iterator<Product> iterator = productData.iterator();
+        while (iterator.hasNext()) {
+            Product existingProduct = iterator.next();
+            if (existingProduct.getProductId().equals(product.getProductId())) {
+                existingProduct.setProductName(product.getProductName());
+                existingProduct.setProductQuantity(product.getProductQuantity());
+                return existingProduct;
+            }
+        }
+        throw new RuntimeException("Product not found");
     }
 }
