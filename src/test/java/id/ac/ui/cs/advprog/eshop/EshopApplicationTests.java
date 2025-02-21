27cd311149ff73a1fc8828f4648272bc18f2ee94
Kae20042005 <farrelilyasa200405@gmail,com>
2025-02-21 18:57:52 +0700

    add build.yml and some unit test

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -2,12 +2,28 @@ package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.boot.SpringApplication;
+
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class EshopApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testMain() {
+        try (MockedStatic<SpringApplication> mockedSpringApp = Mockito.mockStatic(SpringApplication.class)) {
+            String[] args = {};
+            EshopApplication.main(args);
+
+            // Verify that SpringApplication.run() was called with EshopApplication.class and args
+            mockedSpringApp.verify(() -> SpringApplication.run(EshopApplication.class, args));
+        }
     }
 
 }
